1f038d13a5130fdd9fb069e448994401ea11b0a6
Andrei <dernovaw@gmail.com>
2022-01-02 14:30:24 -0600

    Initial Commit

---
+++ FirstProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProj
+//
+//  Created by Andrey Dernov on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
